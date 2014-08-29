057cf5ea0a3a5889474e8065e23cc8db3232556d
eclifford <ericgclifford@gmail.com>
2014-08-28 17:38:26 -0700

    v2.0.16

--- Gruntfile.js
+++ Gruntfile.js
@@ -96,6 +96,7 @@ module.exports = function(grunt) {
     bump: {
       options: {
         files: ['package.json', 'bower.json'],
+        updateConfigs: ['pkg'],
         commit: false,
         createTag: false,
         push: false,
